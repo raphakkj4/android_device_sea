12a029e04a5f3fde0dd1d6aa86ffc7668d9a3e72
raphakkj4 <raphaelfox583@gmail.com>
2025-10-16 22:22:57 -0300

    sea: organizing the location of the props
    Signed-off-by: raphakkj4 <raphaelfox583@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,9 +121,9 @@ BOARD_USES_METADATA_PARTITION := true
 TARGET_BOARD_PLATFORM := mt6781
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.mt6781
